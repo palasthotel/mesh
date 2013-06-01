6d9e5e927608b400298b08576f158644d7917cdd
Paul Vorbach <paul@vorb.de>
2013-06-01 12:53:00 +0200

    formatting

--- editor.js
+++ editor.js
@@ -1,12 +1,12 @@
 // Configuration object. Could also be loaded through an extra script or JSON
 // document.
 var conf = {
-  tools: [ 'bold', 'italic', 'link' ]
+  tools : [ 'bold', 'italic', 'link' ]
 };
 
 /**
  * Initialize Mesh WYSIWYG.
- *
+ * 
  * @param editorDiv
  * @param toolsDiv
  * @param [previewDiv]
@@ -19,49 +19,54 @@ function init(conf, editorDiv, toolsDiv, previewDiv) {
 function initTools(conf, toolsDiv, editorDiv) {
   conf.tools = conf.tools || [];
 
-  conf.tools.forEach(function (tool) {
-    var button;
-    switch (tool) {
-      case 'bold':
-        button = '<button data-tag="bold"><strong>B</strong></button>';
-        break;
-      case 'italic':
-        button = '<button data-tag="italic"><em>I</em></button>';
-        break;
-      case 'link':
-        button = '<button data-tag="createLink"><a style="color: blue; text-decoration: underline">Link</a></button>';
-        break;
-      default:
-        return; // skip adding a button, if the tool is unknown
-    };
+  conf.tools
+      .forEach(function(tool) {
+        var button;
+        switch (tool) {
+        case 'bold':
+          button = '<button data-tag="bold"><strong>B</strong></button>';
+          break;
+        case 'italic':
+          button = '<button data-tag="italic"><em>I</em></button>';
+          break;
+        case 'link':
+          button = '<button data-tag="createLink"><a style="color: blue; text-decoration: underline">Link</a></button>';
+          break;
+        default:
+          return; // skip adding a button, if the tool is unknown
+        }
+        ;
 
-    // add the button to toolsDiv
-    toolsDiv.append(button);
-  });
+        // add the button to toolsDiv
+        toolsDiv.append(button);
+      });
 
   // select all of our buttons
   var btns = $('button[data-tag]');
-  for (var i = 0; i < btns.length; i++) {(function (btn) {
-    btn.addEventListener('click', function (e) {
-      var tag = this.getAttribute('data-tag');
-      switch (tag) {
+  for ( var i = 0; i < btns.length; i++) {
+    (function(btn) {
+      btn.addEventListener('click', function(e) {
+        var tag = this.getAttribute('data-tag');
+        switch (tag) {
         case 'createLink':
           var url = prompt('Please specify the URL');
           if (url)
             document.execCommand(tag, false, url);
           break;
-          // case 'insertImage':
-          //   break;
-          // case 'heading':
-          //   break;
+        // case 'insertImage':
+        // break;
+        // case 'heading':
+        // break;
         default:
           // make bold, italic etc.
           document.execCommand(tag, false, this.getAttribute('data-value'));
-      }
+        }
 
-      e.preventDefault(); // prevent default action (usually submitting the form)
-    });
-  }).call(null, btns[i]);}
+        e.preventDefault(); // prevent default action (usually submitting the
+                            // form)
+      });
+    }).call(null, btns[i]);
+  }
 }
 
 function initEditor(conf, editorDiv, previewDiv) {
@@ -81,9 +86,9 @@ function getTextSelection() {
 }
 
 $(document).ready(function() {
-var editorDiv = $('#mesh-editor');
-var toolsDiv = $('#mesh-tools');
-var previewDiv = $('#mesh-preview');
+  var editorDiv = $('#mesh-editor');
+  var toolsDiv = $('#mesh-tools');
+  var previewDiv = $('#mesh-preview');
 
-init(conf, editorDiv, toolsDiv, previewDiv);
+  init(conf, editorDiv, toolsDiv, previewDiv);
 });
