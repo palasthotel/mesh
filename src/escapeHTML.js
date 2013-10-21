6f61cdbd9163288e913c060b06389b908a51a0b2
Paul Vorbach <paul@vorba.ch>
2013-10-21 20:52:26 +0200

    escapeHTML -> escapeXML

--- src/escapeHTML.js
+++
@@ -1,4 +0,0 @@
-function escapeHTML(text) {
-  return text.replace(/&/g, '&amp;').replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
-}
---
+++ src/escapeXML.js
@@ -0,0 +1,4 @@
+function escapeXML(xml) {
+  return xml.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;').replace(/'/g, '&#039;');
+}
