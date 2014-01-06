5405704b4658329cc724b054d5238e5fe91ae2ab
Paul Vorbach <paul@vorba.ch>
2014-01-06 16:15:02 +0100

    Change editor

--- ts/editor.ts
+++ ts/editor.ts
@@ -1,12 +1,12 @@
 import events = require('./events');
 import undo = require('./undo');
-import exc = require('./exceptions');
+import exceptions = require('./exceptions');
 import util = require('./util');
 import consts = require('./consts');
 import config = require('./config');
 import Configuration = config.Configuration;
 import view = require('./view');
-
+import dom = require('./dom');
 
 /**
  * Modular HTML5 WYSIWYG Editor.
@@ -17,23 +17,38 @@ export class Editor extends events.EventEmitter {
   conf: Configuration;
   view: view.View;
 
-  constructor(elem: HTMLElement, conf: Configuration) {
+  constructor(container: HTMLElement, conf: Configuration) {
     super();
 
-    util.requires(elem.nodeName === consts.NODE_NAMES.TEXTAREA,
+    util.requires(container.nodeName === consts.NODE_NAMES.TEXTAREA,
       'not a textarea');
 
-    this.container = elem;
     this.conf = conf;
     this.undo = new undo.UndoStack<string>(conf.undoSize);
 
     // set the view according to configuration
-    if (conf.defaultView === 'contenteditable')
+    if (conf.defaultView === 'contenteditable') {
       this.view = new view.ContentEditableView(this, conf);
-    else if (conf.defaultView === 'textarea')
+      var id = this.container.id;
+      var content = util.xmlDecode(container.innerText);
+      this.container = document.createElement('div');
+      this.setContent(content);
+      this.container.contentEditable = 'true'; // make content editable
+      dom.replaceNode(container, this.container);
+    } else if (conf.defaultView === 'textarea') {
+      this.container = container;
       this.view = new view.TextAreaView(this, conf);
-    else
-      throw new exc.InvalidConfigurationException('no such view: "'
+    } else {
+      throw new exceptions.InvalidConfigurationException('no such view: "'
         + conf.defaultView + '"');
+    }
+  }
+
+  setContent(html: string): void {
+    if (this.view instanceof view.ContentEditableView) {
+      this.container.innerHTML = html;
+    } else if (this.view instanceof view.TextAreaView) {
+      this.container.innerText = html;
+    }
   }
 }
