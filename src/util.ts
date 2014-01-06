0966a49d9a3cef3fa0726ae8715f76d399d97640
Paul Vorbach <paul@vorba.ch>
2014-01-06 20:33:47 +0100

    Functional array functions

--- src/util.ts
+++ src/util.ts
@@ -19,10 +19,6 @@ export function extend(some: any, other: any): any {
   return result;
 }
 
-export function forEach<T>(xs: any, action: (x: T) => void): void {
-  [].forEach.call(xs, action);
-}
-
 export function xmlEncode(xml: string): string {
   return xml.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;').replace(/'/g, '&#039;');
@@ -31,4 +27,33 @@ export function xmlEncode(xml: string): string {
 export function xmlDecode(encoded: string): string {
   return encoded.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
     .replace(/&quot;/g, '"').replace(/&#039;/g, '\'').replace(/&amp;/g, '&');
+}
+
+export function forEach<T>(xs: any, action: (x: T) => void): void {
+  [].forEach.call(xs, action);
+}
+
+/**
+ * Maps a function on every item in the given Array.
+ */
+export function map<T, U>(xs: Array<T>, mapper: (x: T) => U): Array<U> {
+  var result = new Array<U>(xs.length);
+  for (var i = 0; i < xs.length; i++) {
+    result[i] = mapper(xs[i]);
+  }
+  return result;
+}
+
+/**
+ * Filters items from the Array using a given function.
+ */
+export function filter<T>(xs: Array<T>, matcher: (x: T) => boolean): Array<T> {
+  var result = new Array<T>();
+  var x: T = null;
+  for (var i = 0; i < xs.length; i++) {
+    x = xs[i];
+    if (matcher(x))
+      result.push(x);
+  }
+  return xs;
 }
