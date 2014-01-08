744d0e8a2e78b77cbe95afa5dca85ef4aa957cae
Paul Vorbach <paul@vorba.ch>
2014-01-08 15:06:22 +0100

    Fix behavior of undo stack

--- src/undo.ts
+++ src/undo.ts
@@ -2,15 +2,39 @@
  * Generic stack that can hold an undo history.
  */
 export class UndoStack<T> {
+  // points on the element that would be popped next
   private cursor: number;
-  private stack: Array<T>;
 
-  constructor(public size: number = 100, stack: Array<T> = []) {
+  constructor(public size: number = 50, private stack: Array<T> = []) {
     this.cursor = stack.length - 1;
-    this.stack = stack;
   }
 
   pushState(state: T) {
+    // lose states that have been popped before
+    if (this.cursor < this.stack.length - 1) {
+      this.stack = this.stack.slice(0, this.cursor);
+    }
+
+    if (this.cursor === this.size - 1) {
+      // if the stack has reached the maximum size, remove the first item
+      this.stack = this.stack.slice(1);
+    } else {
+      // otherwise, just increment the cursor
+      this.cursor++;
+    }
+
+    // push the new state
     this.stack.push(state);
+  }
+
+  popState(): T {
+    this.cursor--;
+
+    // can't pop state below zero, will return stack[0] instead
+    if (this.cursor < 0) {
+      this.cursor = 0;
+    }
+
+    return this.stack[this.cursor];
   }
 }
