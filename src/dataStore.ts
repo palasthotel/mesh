d3fadfa7c37ac109e57b0cba991fff649dcfd5f9
Paul Vorbach <paul@vorba.ch>
2014-01-09 01:33:41 +0100

    Improve the dataStore

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -4,13 +4,12 @@
 (<any> document).meshDataStore = {};
 
 export function get(elem: HTMLElement): any {
-  return (<any> document).meshDataStore[elem];
-};
+  var data = (<any> document).meshDataStore[elem];
 
-export function set(elem: HTMLElement, data: any): void {
-  (<any> document).meshDataStore[elem] = data;
-};
+  if (typeof data == 'undefined') {
+    data = {};
+    (<any> document).meshDataStore[elem] = data;
+  }
 
-export function unset(elem: HTMLElement): void {
-  delete (<any> document).meshDataStore[elem];
+  return data;
 };
