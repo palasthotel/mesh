ff0bdbabcbd2b3028c29ef295bdb073c8ed8b766
Paul Vorbach <paul@vorba.ch>
2014-01-09 17:40:36 +0100

    Simplify Drag 'n' Drop

--- src/dnd.ts
+++ src/dnd.ts
@@ -1,69 +1,23 @@
 import util = require('./util');
 import dom = require('./dom');
+import dataStore = require('./dataStore');
 
-var defaultConf = {
-  dragClass: 'drag',
-  overClass: 'drag-over',
-  droppableClass: 'droppable',
-  notDroppableClass: 'not-droppable'
-};
-
-export function draggable(elem: HTMLElement, config: {}) {
-  var conf: any = util.extend(defaultConf, config);
-
-  elem.setAttribute('draggable', 'true');
-
-  elem.addEventListener('dragstart', (e) => {
-    console.log('start', e.target);
-    elem.style.color = '#555';
-    dom.addClass(elem, conf.dragClass);
+/**
+ * Make an HTMLElement draggable. You can give it an optional handle.
+ */
+export function makeDraggable(elem: HTMLElement, handle: HTMLElement = elem): Draggable {
+  dom.addEventListener(elem, 'mousedown', (e) => {
+    elem.draggable = true;
   });
 
-  elem.addEventListener('dragenter', (e) => {
-    console.log('enter', e.target);
+  dom.addEventListener(elem, 'dragstart', (e) => {
+    elem.draggable = false;
   });
 
-  elem.addEventListener('dragleave', (e) => {
-    console.log('leave', e.target);
-  });
-
-  // dragstart
-  // drag
-  // dragenter
-  // dragleave
-  // dragover
-  // drop
-  // dragend
+  return new Draggable(elem, handle);
 }
 
 export class Draggable {
-  private elem: HTMLElement;
-  private enabled = true;
-
-  constructor(elem: HTMLElement) {
-    elem.setAttribute('draggable', 'true');
-    elem.addEventListener('dragstart', this.onDragStart);
-    elem.addEventListener('dragend', this.onDragEnd);
-  }
-
-  private onDragStart(e: DragEvent): void {
-    console.log(e);
-  }
-
-  private onDragEnd(e: DragEvent): void {
-    console.log(e);
-  }
-}
-
-export class Droppable {
-  private elem: HTMLElement;
-  private enabled = true;
-
-  constructor(elem: HTMLElement) {
-    this.elem = elem;
-  }
-
-  setEnabled(value: boolean): void {
-    this.enabled = value;
+  constructor(public elem: HTMLElement, public handle: HTMLElement) {
   }
 }
