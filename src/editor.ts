dfec29375410e856836ca1a0f950d0228a20a01c
Paul Vorbach <paul@vorba.ch>
2014-01-09 17:41:16 +0100

    Improve editor code

--- src/editor.ts
+++ src/editor.ts
@@ -5,50 +5,94 @@ import util = require('./util');
 import consts = require('./consts');
 import config = require('./config');
 import Configuration = config.Configuration;
-import view = require('./view');
 import dom = require('./dom');
+import model = require('./model');
+import dataStore = require('./dataStore');
 
 /**
- * Modular HTML5 WYSIWYG Editor.
+ * Modular HTML5 WYSIWYG Editor (Controller).
  */
 export class Editor extends events.EventEmitter {
-  container: HTMLElement;
+  elem: HTMLElement;
   undo: undo.UndoStack<string>;
   conf: Configuration;
-  view: view.View;
+
+  // IMPORTANT this attribute can be null!
+  doc: model.Document = null;
 
   constructor(container: HTMLElement, conf: Configuration) {
     super();
 
-    util.requires(container.nodeName === consts.NODE_NAMES.TEXTAREA,
+    util.requires(container.nodeName === 'TEXTAREA',
       'not a textarea');
 
     this.conf = conf;
     this.undo = new undo.UndoStack<string>(conf.undoSize);
 
     // set the view according to configuration
-    if (conf.defaultView === 'contenteditable') {
-      this.view = new view.ContentEditableView(this, conf);
-      var id = this.container.id;
-      var content = util.xmlDecode(container.innerText);
-      this.container = document.createElement('div');
+    if (conf.defaultView === 'textarea') {
+      // use the textarea as-is
+      this.elem = container;
+    } else if (conf.defaultView === 'contenteditable') {
+      // use the id of the textarea as the new id of the container
+      var id = container.id;
+      this.elem = document.createElement('div');
+      this.elem.id = id;
+
+      // decode the existing content of the textarea
+      var content = util.xmlDecode(container.textContent);
       this.setContent(content);
-      this.container.contentEditable = 'true'; // make content editable
-      dom.replaceNode(container, this.container);
-    } else if (conf.defaultView === 'textarea') {
-      this.container = container;
-      this.view = new view.TextAreaView(this, conf);
+
+      // replace the container node
+      dom.replaceNode(container, this.elem);
+
+      // make content editable
+      this.elem.contentEditable = 'true';
     } else {
+      // unknown case
       throw new exceptions.InvalidConfigurationException('no such view: "'
         + conf.defaultView + '"');
     }
+
+    // save a ref to this editor
+    dataStore.get(this.elem).editor = this;
+
+    // on every change of the content, push a new state to the undo stack
+    this.addListener('change', () => {
+      var content = this.getContent();
+      this.undo.pushState(content);
+    });
+
+    // emit change event after a short delay
+    setTimeout(() => {
+      this.emit('change');
+    }, 0);
   }
 
-  setContent(html: string): void {
-    if (this.view instanceof view.ContentEditableView) {
-      this.container.innerHTML = html;
-    } else if (this.view instanceof view.TextAreaView) {
-      this.container.innerText = html;
+  setContent(content: string): void {
+    if (dom.hasType(this.elem, 'TEXTAREA')) {
+      // straight forward
+      this.elem.textContent = content;
+    } else {
+      this.setDocument(new model.Document(this.elem, content));
+    }
+
+    this.emit('change');
+  }
+
+  setDocument(doc: model.Document): void {
+    this.doc = doc;
+  }
+
+  getDocument(): model.Document {
+    return this.doc;
+  }
+
+  getContent(): string {
+    if (dom.hasType(this.elem, 'TEXTAREA')) {
+      return this.elem.textContent;
+    } else {
+      return this.elem.innerHTML;
     }
   }
 }
