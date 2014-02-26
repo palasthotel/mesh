087bb95c4f23ab368ef16e639d1b3f9f9273ce00
Paul Vorbach <paul@vorba.ch>
2014-02-26 17:59:24 +0100

    Editor

--- src/editor.js
+++ src/editor.js
@@ -1,428 +1,143 @@
 /**
- * Creates a new `Editor`.
+ * WYSIWYG Editor.
  * 
- * @module Editor
+ * @module editor
  * 
- * @class
- * @classdesc Editor
- * 
- * @constructor
- * @param {HTMLElement} elem - `<textarea>` element
- * @param {Attributes} attrs
- * @param {Array} [historyStack]
- * @param {Number} [historySize]
+ * @author Paul Vorbach
+ * @since 0.0.1
  */
-module.exports = function Editor(elem, attrs, historyStack, historySize) {
-  if (!(this instanceof Editor))
-    return new Editor(elem, attributes, historyStack, historySize);
-  if (!elem)
-    throw new TypeError('expects an element');
 
-  var editor = this; // ref to self
+var events = require('events');
+var dom = require('./dom.js');
+var oo = require('./oo.js');
+var util = require('./util.js');
+var undo = require('./undo.js');
 
-  this.history = new History(historyStack || []);
-  this.history.setMaximumEntries(historySize || 100);
+exports.Editor = Editor;
 
-  this.container = elem;
-  this.attributes = attributes;
+/**
+ * Modular HTML5 WYSIWYG Editor.
+ * 
+ * @class Editor
+ * @constructor
+ * 
+ * @param {HTMLElement} container - surrounding element
+ * @param {Object} conf - configuration
+ * 
+ * @extends EventEmitter
+ * 
+ * @since 0.0.1
+ */
+function Editor(container, conf) {
+  EventEmitter.call(this);
 
-  this.setEnabled(true);
+  util.requires(dom.hasType(container, 'TEXTAREA'), 'not a <textarea>');
 
-  this.blocks = [];
-  this.activeBlock = null;
+  this.conf = conf;
+  // undoStack
+  this.undo = new undo.UndoStack(conf.undoSize);
 
-  var i, len = this.container.children.length;
-  for (i = 0; i < len; i++) {
-    this.blocks.push(new Block(this, this.container.children[i]));
+  // set the view according to configuration
+  if (conf.defaultView === 'textarea') {
+    // use the textarea as-is
+    this.elem = container;
+  } else if (conf.defaultView === 'contenteditable') {
+    // use the id of the textarea as the new id of the container
+    var id = container.id;
+    this.elem = document.createElement('div');
+    this.elem.id = id;
+
+    // decode the existing content of the textarea
+    var content = util.xmlDecode(container.textContent);
+    this.setContent(content);
+
+    // replace the container node
+    dom.replaceNode(container, this.elem);
+
+    // make content editable
+    this.elem.contentEditable = 'true';
+  } else {
+    // unknown case
+    throw new exceptions.InvalidConfigurationException('no such view: "'
+        + conf.defaultView + '"');
   }
 
-  $(this.container).sortable({
-    placeholder : 'placeholder', // css class .placeholder
-    axis : 'y',
-    receive : function(e, ui) {
-      console.log(e, ui);
-      editor.emitEvent('add-block');
-    },
-    stop : function(e) {
-      rangy.getSelection().removeAllRanges();
-      editor.updateBlocks();
-      editor.emitEvent('sort');
-    }
+  // save a ref to this editor
+  dataStore.get(this.elem).editor = this;
+
+  // on every change of the content, push a new state to the undo stack
+  this.addListener('change', function() {
+    var content = this.getContent();
+    this.undo.pushState(content);
   });
 
-  // update active block
-  $(this.container).bind('click keydown', function() {
-    var sel = rangy.getSelection();
-    var block = editor.getSelectedBlock(sel);
-    if (block !== null)
-      editor.activeBlock = block;
-  });
-}
-
-// Event emitter
-Emitter(Editor.prototype);
-
-Editor.prototype.setContent = function setContent(html) {
-  var editor = this;
-  if (typeof html != 'string')
-    throw new Error('not a string');
-
-  var c = document.createElement('div');
-  c.innerHTML = html;
-
-  var i, len = c.children.length;
-  for (i = 0; i < len; i++) {
-    var node = c.children[i];
-    var type = node.nodeName.toLowerCase();
-    var block = Block.create(editor, type);
-
-    var html = node.innerHTML;
-    block.setHTMLContent(html);
-
-    this.appendBlock(block);
-  }
-};
-
-Editor.prototype.appendBlock = function appendBlock(block) {
-  this.blocks.push(block);
-  this.container.appendChild(block.node);
+  // emit change event after a short delay
+  setTimeout(function() {
+    this.emit('change');
+  }, 0);
 };
 
 /**
- * Get editor contents.
+ * Checks, whether the view of this editor is the given `viewType`.
  * 
- * @return {String}
+ * @param {String} viewType - view type of interest, either `'textarea'` or
+ *                `'contenteditable'`
+ * 
+ * @returns {Boolean}
+ */
+Editor.prototype.isView = function(viewType) {
+  if (viewType === 'textarea') {
+    return dom.hasType(this.elem, 'TEXTAREA');
+  } else if (viewType === 'contenteditable') {
+    return dom.hasType(this.elem, 'DIV');
+  }
+
+  return false;
+};
+
+/**
+ * Set the editor's contents.
+ * 
+ * @param {String} content - HTML string
+ */
+Editor.prototype.setContent = function(content) {
+  if (this.isView('textarea')) {
+    // straight forward
+    this.elem.textContent = content;
+  } else {
+    this.setDocument(new model.Document(this.elem, content));
+  }
+
+  this.emit('change');
+};
+
+/**
+ * Get the editor's contents.
+ * 
+ * @returns {String}
  */
 Editor.prototype.getContent = function() {
-  return this.container.innerHTML;
-};
-
-/**
- * Is editor enabled?
- * 
- * @return {Boolean}
- */
-Editor.prototype.isEnabled = function() {
-  return this.container.contentEditable == 'true';
-};
-
-/**
- * Toggle editor state.
- * 
- * @return {Editor}
- */
-Editor.prototype.toggleEnabled = function() {
-  if (this.isEnabled())
-    this.setEnabled(false)
-  else
-    this.setEnabled(true);
-
-  return this;
-};
-
-/**
- * Enable or disable editing.
- * 
- * @return {Editor}
- */
-Editor.prototype.setEnabled = function(bool) {
-  var $c = $(this.container);
-  if (bool) {
-    this.container.contentEditable = true;
-    $c.bind('keydown', $.proxy(this.onkeydown, this));
-    $c.bind('keyup click focus', $.proxy(this.onstatechange, this));
-    $c.bind('paste input', $.proxy(this.onchange, this));
-    this.emitEvent('enabled');
+  if (this.isView('textarea')) {
+    return this.elem.textContent;
   } else {
-    this.container.contentEditable = false;
-    $c.unbind();
-    this.emitEvent('disabled');
+    return this.elem.innerHTML;
   }
-
-  return this;
-};
-
-Editor.prototype.getActiveBlock = function() {
-  return this.activeBlock;
-};
-
-Editor.prototype.getSelectedBlock = function(selection) {
-  var n = selection.anchorNode;
-
-  while (!n.classList || !n.classList.contains('block')) {
-    n = n.parentNode;
-  }
-
-  var i = 0;
-  var len = this.blocks.length;
-  var block = null;
-  for (i = 0; i < len; i++) {
-    block = this.blocks[i];
-    if (block.node === n)
-      return block;
-  }
-
-  return null;
 };
 
 /**
- * Undo.
+ * Set the editor's document.
  * 
- * @return {Editor}
+ * @param {Document} doc - document
  */
-Editor.prototype.undo = function() {
-  var buf = this.history.prev();
-  this.container.innerHTML = buf || this.container.innerHTML;
-  buf || this.emit('state');
-  return this;
+Editor.prototype.setDocument = function(doc) {
+  this.doc = doc;
 };
 
 /**
- * Redo.
+ * Get the editor's document.
  * 
- * @return {Editor}
+ * @returns {Document}
  */
-Editor.prototype.redo = function() {
-  var buf = this.history.next();
-  var curr = this.container.innerHTML;
-  this.container.innerHTML = buf || curr;
-  buf || this.emit('state');
-  return this;
-};
-
-/**
- * Execute the given `cmd` with `val`.
- * 
- * @param {String} cmd
- * @param {Mixed} val
- * @return {Editor}
- */
-Editor.prototype.execute = function(cmd, val) {
-  document.execCommand(cmd, false, val);
-  this.onstatechange();
-  return this;
-};
-
-function isChildOf(node, ancestorName) {
-  if (!node)
-    return false;
-  if (node.nodeType === 1 && node.nodeName === ancestorName)
-    return true;
-  if (!node.parentNode)
-    return false;
-  return isChildOf(node.parentNode, ancestorName);
-}
-
-/**
- * Query `cmd` state.
- * 
- * @param {String} cmd
- * @return {Boolean}
- */
-Editor.prototype.getState = function(cmd) {
-  var node = rangy.getSelection().anchorNode;
-  if (cmd === 'link') {
-    return isChildOf(node, 'A');
-  } else if (cmd === 'p') {
-    return isChildOf(node, 'P');
-  } else if (cmd === 'quote') {
-    return isChildOf(node, 'BLOCKQUOTE');
-  } else if (cmd === 'ul') {
-    return isChildOf(node, 'UL');
-  } else if (cmd === 'ol') {
-    return isChildOf(node, 'OL');
-  } else if (cmd === 'code') {
-    return isChildOf(node, 'PRE');
-  }
-
-  var length = this.history.vals.length - 1;
-  var stack = this.history;
-
-  if (cmd === 'undo')
-    return 0 < stack.i;
-  if (cmd === 'redo')
-    return length > stack.i;
-
-  return document.queryCommandState(cmd);
-};
-
-/**
- * Emit `state`.
- * 
- * @param {Event} e
- * @return {Editor}
- */
-Editor.prototype.onstatechange = function(e) {
-  this.emitEvent('state', e);
-  return this;
-};
-
-/**
- * Emit `change` and push current `buf` to history.
- * 
- * @param {Event} e
- * @return {Editor}
- */
-Editor.prototype.onchange = function(e) {
-  this.history.add(this.getContent());
-
-  this.emitEvent('change', e);
-  return this;
-};
-
-Editor.prototype.onkeydown = function(e) {
-  console.log(e.keyCode);
-
-  if (e.keyCode === 13 || e.keyCode === 10) { // return/enter key
-    var sel = rangy.getSelection();
-
-    // TODO handle non-collapsed selections
-    if (!sel.isCollapsed)
-      throw new Error('not collapsed! TO BE HANDLED');
-
-    prevent(e); // prevent default browser behaviour
-
-    var block = this.getSelectedBlock(sel);
-    // If the caret is behind another block, insert a new block
-    if (Block.isSelectionAfterBreak(sel)) {
-      console.log('block');
-      block.insertBreak(sel);
-    } else { // otherwise insert a line break
-      // insert line break
-      block.insertBreak(sel);
-    }
-  }
-  return this;
-};
-
-function isEmptyNode(node) {
-  return node !== null && node.children && node.children.length === 1
-      && node.children[0].nodeName === 'BR';
-}
-
-function isNthChild(node, n) {
-  var siblings = node.parentNode.children;
-  var nth = siblings[n - 1];
-  return node === nth;
-}
-
-function isLastChild(node) {
-  var siblings = node.parentNode.children;
-  return siblings[siblings.length - 1] === node;
-}
-
-function prevent(e) {
-  e.preventDefault();
-}
-
-Editor.createBreak = function createBreak() {
-  var brk = document.createElement('div');
-  brk.classList.add('break');
-  return brk;
-};
-
-Editor.createMain = function createMain(clss) {
-  var main = document.createElement('div');
-  main.classList.add(clss);
-  return main;
-};
-
-Editor.createBlock = function createBlock(main) {
-  var block = document.createElement('div');
-  var handle = document.createElement('div');
-  var controls = document.createElement('div');
-
-  block.classList.add('block');
-  handle.classList.add('handle');
-  controls.classList.add('controls');
-
-  block.appendChild(handle);
-  block.appendChild(main);
-  block.appendChild(controls);
-
-  return {
-    block : block,
-    handle : handle,
-    main : main,
-    controls : controls
-  };
-};
-
-Editor.createEmptyBlock = function createEmptyBlock(clss) {
-  var main = Editor.createMain('paragraph');
-  main.innerHTML = '<br />';
-  return Editor.createBlock(main);
-};
-
-Editor.prototype.insertBlockAfter = function insertBlockAfter(block) {
-
-  console.log('inserted')
-  // block.node
-};
-
-Editor.prototype.insertBlockAt = function insertBlockAt(i, block) {
-
-};
-
-Editor.prototype.updateBlocks = function updateBlocks() {
-  var i, j;
-  var block = null;
-
-  var bs = this.blocks.slice();
-
-  for (i = 0; i < bs.length; i++) {
-    block = bs[i];
-
-    for (j = 0; j < bs.length; j++) {
-      if (block.node === this.container.children[j]) {
-        this.blocks[j] = block;
-        break;
-      }
-    }
-  }
-};
-
-Editor.prototype.editAttributes = function editAttributes(block) {
-  this.activeBlock = block;
-  this.attributes.setAttributes(block.attributes);
-  this.attributes.setVisible(true);
-};
-
-Editor.prototype.getWordCount = function getWordCount() {
-  var i = 0;
-  var total = 0;
-  var block = null;
-
-  for (i = 0; i < this.blocks.length; i++) {
-    block = this.blocks[i];
-    total += block.getWordCount();
-  }
-
-  return total;
-};
-
-Editor.prototype.toHTML = function toHTML() {
-  var i = 0;
-  var source = [];
-  var block = null;
-
-  for (; i < this.blocks.length; i++) {
-    block = this.blocks[i];
-    source.push(block.toHTML());
-  }
-
-  return source.join('\n');
-};
-
-Editor.prototype.toXML = function toXML() {
-  var i = 0;
-  var source = [];
-  var block = null;
-
-  for (; i < this.blocks.length; i++) {
-    block = this.blocks[i];
-    source.push(block.toXML());
-  }
-
-  return source.join('\n');
+Editor.prototype.getDocument = function() {
+  return this.doc;
 };
