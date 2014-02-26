781c0711b2c16278607fa3e31c7bf4f7c3654373
Paul Vorbach <paul@vorba.ch>
2014-02-26 13:45:26 +0100

    OO extension/inheritance

--- src/oo.js
+++ src/oo.js
@@ -5,7 +5,8 @@
  */
 
 /**
- * Object.create();
+ * Same as `Object.create(proto)`. Used since older browsers don't support
+ * `Object.create()` _out of the box_.
  * 
  * @method
  * 
@@ -16,8 +17,41 @@
  * 
  * @since 0.0.1
  */
-exports.createObject = Object.create || function(o) {
+exports.createObject = Object.create || function(proto) {
   var T = function() {};
-  T.prototype = o;
+  T.prototype = proto;
   return new T();
+};
+
+/**
+ * `Sub` extends `Super`, i.e. `Sub` inherits all members from `Super`.
+ * (Pseudoclass inheritance)
+ * 
+ * @param {*} Sub - child
+ * @param {*} Super - parent
+ * 
+ * @since 0.0.1
+ * @see http://molily.de/weblog/javascript-pseudoklassen
+ * 
+ * @example
+ * 
+ * var oo = require('./oo.js');
+ * 
+ * function SuperClass(a) {
+ *   this._a = a;
+ * }
+ * 
+ * // You need to call the constructor of SuperClass,
+ * // otherwise attributes may not get initialized
+ * // properly.
+ * function SubClass(a, b) {
+ *   SuperClass.call(this, a);
+ *   this._b = b;
+ * }
+ * 
+ * oo.extends(SubClass, SuperClass);
+ */
+exports.extends = function(SubClass, SuperClass) {
+  SubClass.prototype = exports.createObject(SuperClass.prototype);
+  SubClass.prototype.constructor = SubClass;
 };
