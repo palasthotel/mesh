4bcfd2a7cc09d90595b11695d68e3844b465694a
Paul Vorbach <paul@vorba.ch>
2014-02-27 11:42:18 +0100

    Default configuration

---
+++ src/config.js
@@ -0,0 +1,24 @@
+/**
+ * Configuration related.
+ * 
+ * @module config
+ */
+
+/**
+ * Default configuration.
+ * 
+ *     DEFAULT = {
+ *       undoSize : 50,
+ *       undoDelay : 700,
+ *       statusDelay : 1000,
+ *       defaultView : 'contenteditable'
+ *     }
+ * 
+ * @type {Object}
+ */
+exports.DEFAULT = {
+  undoSize : 50,
+  undoDelay : 700,
+  statusDelay : 1000,
+  defaultView : 'contenteditable'
+};
