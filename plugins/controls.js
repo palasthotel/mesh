0afd09eea7d7c8a98cd3e099f1b242b37af7bb9d
Paul Vorbach <paul@vorba.ch>
2014-03-24 16:31:12 +0100

    plugins/buttons.js -> plugins/controls.js

---
+++ plugins/controls.js
@@ -0,0 +1,5 @@
+var mesh = require('../src/index.js');
+var oo = mesh.oo;
+
+var controls = module.exports = [];
+
--- plugins/index.js
+++ plugins/index.js
@@ -1,2 +1,2 @@
 exports.blocks = require('./blocks.js');
-exports.buttons = require('./buttons.js');
+exports.buttons = require('./controls.js');
