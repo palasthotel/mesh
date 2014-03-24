daf96390a7bef3b64993f8d5f5ef54ad17060d65
Paul Vorbach <paul@vorba.ch>
2014-03-24 13:22:08 +0100

    Export view and model

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ exports.config = require('./config.js');
 exports.dom = require('./dom.js');
 exports.editor = require('./editor.js');
 exports.init = require('./init.js');
+exports.model = require('./model.js');
 exports.util = require('./util.js');
+exports.view = require('./view.js');
