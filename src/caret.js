939e16fad9b694928e35c34fab755c2ebbab9ccc
Paul Vorbach <paul@vorba.ch>
2014-03-25 11:21:54 +0100

    Adapt deprecated code

--- src/caret.js
+++ src/caret.js
@@ -1,41 +1,31 @@
-(function() {
-  'use strict';
+var caret = exports;
 
-  var Caret = {};
+/**
+ * Moves the caret to the beginning of the given node.
+ */
+caret.moveToBeginning = function moveToBeginning(node, sel) {
+  var range = rangy.createRange();
+  range.setStart(node, 0);
+  range.collapse(true);
+  sel.removeAllRanges();
+  sel.addRange(range);
+};
 
-  // export functionality
-  if (typeof module === 'object')
-    module.exports = Caret;
-  else
-    window.Caret = Caret;
+/**
+ * Moves the caret to the ending of the given node.
+ */
+caret.moveToEnding = function moveToEnding(node, sel) {
+  var range = rangy.createRange();
+  var pos = 0;
 
-  /**
-   * Moves the caret to the beginning of the given node.
-   */
-  Caret.moveToBeginning = function moveToBeginning(node, sel) {
-    var range = rangy.createRange();
-    range.setStart(node, 0);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
-  };
-
-  /**
-   * Moves the caret to the ending of the given node.
-   */
-  Caret.moveToEnding = function moveToEnding(node, sel) {
-    var range = rangy.createRange();
-    var pos = 0;
-
-    if (node.nodeType === 3)
-      pos = node.length;
-    else {
-      pos = node.childNodes.length;
-    }
-
-    range.setStart(node, pos);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
+  if (node.nodeType === 3)
+    pos = node.length;
+  else {
+    pos = node.childNodes.length;
   }
-})();
+
+  range.setStart(node, pos);
+  range.collapse(true);
+  sel.removeAllRanges();
+  sel.addRange(range);
+};
--- src/oembed.js
+++ src/oembed.js
@@ -1,93 +1,82 @@
-(function(window) {
+var oembed = exports;
 
-  if (typeof $ === 'undefined')
-    throw new Error('jQuery undefined')
+/**
+ * Array of all oEmbed providers.
+ */
+var providers = oembed.providers = [];
 
-  var oembed = {};
+oembed.providers.push({
+  scheme : 'http://www.flickr.com/photos/*',
+  endpoint : 'http://www.flickr.com/services/oembed/',
+  callbackParameter : 'jsoncallback'
+});
 
-  /**
-   * Array of all oEmbed providers.
-   */
-  var providers = oembed.providers = [];
+/**
+ * Get the corresponding oEmbed provider for a given URL.
+ * 
+ * @returns a provider object, if the URL is embeddable, false otherwise
+ */
+oembed.getProviderFor = function getProviderFor(url) {
+  var i, provider, regex;
 
-  oembed.providers.push({
-    scheme : 'http://www.flickr.com/photos/*',
-    endpoint : 'http://www.flickr.com/services/oembed/',
-    callbackParameter : 'jsoncallback'
-  });
+  // for every provider, check if the given url matches the provider's scheme.
+  for (i = 0; i < providers.length; i++) {
+    provider = providers[i];
+    var scheme = provider.scheme;
+    regex = new RegExp(scheme.replace('*', '.+'))
+    if (regex.test(url))
+      return provider;
+  }
 
-  /**
-   * Get the corresponding oEmbed provider for a given URL.
-   * 
-   * @returns a provider object, if the URL is embeddable, false otherwise
-   */
-  oembed.getProviderFor = function getProviderFor(url) {
-    var i, provider, regex;
+  return false;
+};
 
-    // for every provider, check if the given url matches the provider's scheme.
-    for (i = 0; i < providers.length; i++) {
-      provider = providers[i];
-      var scheme = provider.scheme;
-      regex = new RegExp(scheme.replace('*', '.+'))
-      if (regex.test(url))
-        return provider;
-    }
+/**
+ * Request oEmbed information.
+ * 
+ * @param {String} url - the url you want to embed
+ * @param {Object} requestParams - an object that contains additional request
+ *                parameters (such as maxwidth, maxheight)
+ * @param {Function} callback - a callback function(err, info)
+ */
+oembed.request = function request(url, requestParams, callback) {
+  var provider = oembed.getProviderFor(url);
+  if (provider === false)
+    return callback(new Error('not embeddable'));
 
-    return false;
-  };
+  var requestUrl = provider.endpoint;
+  // set the query parameters of the request url
+  requestUrl += '?url=' + encodeURI(url);
+  var keys = Object.keys(requestParams);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    requestUrl += '&' + key + '=' + requestParams[key];
+  }
+  if (typeof provider.callbackParameter !== 'undefined')
+    requestUrl += '&' + provider.callbackParameter + '=?';
 
-  /**
-   * Request oEmbed information.
-   * 
-   * @param url
-   *                the url you want to embed
-   * @param requestParams
-   *                an object that contains additional request parameters (such
-   *                as maxwidth, maxheight)
-   * @param callback
-   *                a callback function(err, info)
-   */
-  oembed.request = function request(url, requestParams, callback) {
-    var provider = oembed.getProviderFor(url);
-    if (provider === false)
-      return callback(new Error('not embeddable'));
+  function request(dataType) {
+    $.ajax({
+      url : requestUrl,
+      type : 'GET',
+      dataType : dataType,
+      success : function(data, textStatus, jqXHR) {
+        callback(null, data);
+      },
+      error : function(jqXHR, textStatus, errorThrown) {
+        callback(errorThrown);
+      }
+    });
+  }
 
-    var requestUrl = provider.endpoint;
-    // set the query parameters of the request url
-    requestUrl += '?url=' + encodeURI(url);
-    var keys = Object.keys(requestParams);
-    for ( var i = 0; i < keys.length; i++) {
-      var key = keys[i];
-      requestUrl += '&' + key + '=' + requestParams[key];
-    }
-    if (typeof provider.callbackParameter !== 'undefined')
-      requestUrl += '&' + provider.callbackParameter + '=?';
+  try {
+    request('json');
+  } catch (err) {
+    request('jsonp');
+  }
+};
 
-    function request(dataType) {
-      $.ajax({
-        url : requestUrl,
-        type : 'GET',
-        dataType : dataType,
-        success : function(data, textStatus, jqXHR) {
-          callback(null, data);
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-          callback(errorThrown);
-        }
-      });
-    }
-
-    try {
-      request('json');
-    } catch (err) {
-      request('jsonp');
-    }
-  };
-
-  oembed.discover = function discover(url, callback) {
-    // does nothing at the moment
-    callback(new Error('not impelemented'));
-  };
-
-  window.oembed = oembed;
-})(this);
+oembed.discover = function discover(url, callback) {
+  // does nothing at the moment
+  callback(new Error('not impelemented'));
+};
